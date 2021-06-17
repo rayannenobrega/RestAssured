743bc696efaa423d9cf10fea265b4ddca38d5aae
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-06-17 11:55:53 -0300

    Primeiro Teste no terminal

--- src/main/java/br/com/rayanne/rest/Ola.java
+++ src/main/java/br/com/rayanne/rest/Ola.java
@@ -3,13 +3,18 @@ package br.com.rayanne.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class Ola {
     public static void main(String[] args) {
         Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-        System.out.println(response.getBody().asString());
-        System.out.println(response.statusCode());
+        System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+        System.out.println(response.statusCode() == 200);
 
+        //ferramenta de validação do próprio RestAssured - A partir da resposta se faz um then. O then devolve uma validatableResponse que chamamos de validacao.
+        //A partir da response obtemos um status code. A partir da validação passamos um statuscode e verificamos se o satatuscode passado é igual.
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
     }
 
 }
