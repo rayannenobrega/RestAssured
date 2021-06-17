fbd36aa9b652bebdc22d2b3152bebb1cadd80d99
Rayanne Nóbrega <nanyz.nobrega@gmail.com>
2021-06-17 15:16:11 -0300

    Validando o Body

--- src/main/java/br/com/rayanne/rest/OlaMundoTest.java
+++ src/main/java/br/com/rayanne/rest/OlaMundoTest.java
@@ -76,4 +76,16 @@ public class OlaMundoTest {
         assertThat("Maria", anyOf(is("Maria"), is("Joaquina")));
         assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
     }
+
+    @Test
+    public void devoValidarBody(){
+        given()
+        .when()
+                .get("http://restapi.wcaquino.me/ola")
+        .then()
+                .statusCode(200)
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(not(nullValue())));
+    }
 }
