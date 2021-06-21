88e07ef42c888312ea07109c4869872b74749a40
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-21 13:47:09 -0300

    nó raiz

--- src/main/java/br/com/rayanne/rest/UserXMLTest.java
+++ src/main/java/br/com/rayanne/rest/UserXMLTest.java
@@ -16,13 +16,20 @@ public class UserXMLTest {
                 .get("https://restapi.wcaquino.me/usersXML/3")
         .then()
                 .statusCode(200)
-                .body("user.name", is("Ana Julia"))
-                .body("user.@id", is("3"))
-                .body("user.filhos.name.size()", is(2)) // aqui não se usa "" porque esse dois não está vindo como um valor do XML, ele é seu tamanho relativo a uma chave do XML.
-                .body ("user.filhos.name[0]",is("Zezinho"))
-                .body ("user.filhos.name[1]",is("Luizinho"))
-                .body ("user.filhos.name",hasItem("Luizinho"))
-                .body ("user.filhos.name",hasItems("Luizinho", "Zezinho"))
+                .rootPath("user")
+                .body("name", is("Ana Julia"))
+                .body("@id", is("3"))
+
+                .rootPath("user.filhos")//define uma rota
+                .body("name.size()", is(2)) // aqui não se usa "" porque esse dois não está vindo como um valor do XML, ele é seu tamanho relativo a uma chave do XML.
+
+                .detachRootPath("filhos")//retira um nó da rota
+                .body ("filhos.name[0]",is("Zezinho"))
+                .body ("filhos.name[1]",is("Luizinho"))
+
+                .appendRootPath("filhos")//acrescenta um nó da rota
+                .body ("name",hasItem("Luizinho"))
+                .body ("name",hasItems("Luizinho", "Zezinho"))
                 ;
     }
 }
