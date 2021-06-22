dc124945567e7217b7a5946a9d2e1ffcd0463cf3
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-22 12:46:52 -0300

    validação ao salvar

--- src/main/java/br/com/rayanne/rest/VerbosTeste.java
+++ src/main/java/br/com/rayanne/rest/VerbosTeste.java
@@ -25,5 +25,22 @@ public class VerbosTeste {
                 .body("age", is(50))
         ;
     }
+
+    @Test
+    public void naoDeveSalvarUsuarioSemNome(){
+        given()
+                .log().all()
+                .contentType("application/json")
+                .body("{\"age\":50}")
+        .when()
+                .post("https://restapi.wcaquino.me/users")
+        .then()
+                .log().all()
+                .statusCode(400)
+                .body("id", is(nullValue()))
+                .body("error", is("Name é um atributo obrigatório"))
+
+        ;
+    }
 }
 
