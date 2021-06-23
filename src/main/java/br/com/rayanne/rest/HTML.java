b33edf26db5d89b9dff0cca39b8557f2a64425a6
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-23 11:42:46 -0300

    XPath com HTML

--- src/main/java/br/com/rayanne/rest/HTML.java
+++ src/main/java/br/com/rayanne/rest/HTML.java
@@ -24,7 +24,23 @@ public class HTML {
                 .body("html.body.div.table.tbody.tr.size()", is(3))
                 .body("html.body.div.table.tbody.tr[1].td[2]", is("25"))
                 .appendRootPath("html.body.div.table.tbody")
-                .body("tr.find{it.toString().startsWith('2)}.td[1]", is("Maria Joaquina")) //usando o iterador facilita, lembra um xpath.
+                .body("tr.find{it.toString().startsWith('2)}.td[1]", is("Maria Joaquina")) //usando o iterador facilita {it}.
+        ;
+    }
+
+    @Test
+    public void deveFazerBuscasComXpathEmHTML(){
+        //As vezes não é possível trabalhar com XPath porque o HTML não está com todas as exigências que o xpath solicita. Por isso estamos usando o formato clean de tabela nesse teste.
+        given()
+                .log().all()
+                .when()
+                .get("https://restapi.wcaquino.me/v2/users?format=clean")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.HTML)
+                .body(hasXPath("count(//table/tr)", is("4")))
+                .body(hasXPath("//td[text() = '2']/../td[2]", is("Maria Joaquina")))
         ;
     }
 }
