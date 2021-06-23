8143c6b42b99f691f494fd84cf73606e62b1e7cb
rayannenobrega <nanyz.nobrega@gmail.com>
2021-06-23 09:37:16 -0300

    Query parametrizável

--- src/main/java/br/com/rayanne/rest/EnvioDadosTest.java
+++ src/main/java/br/com/rayanne/rest/EnvioDadosTest.java
@@ -2,6 +2,7 @@ package br.com.rayanne.rest;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.*;
@@ -19,6 +20,22 @@ public class EnvioDadosTest {
                 .log().all()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
+        ;
+    }
+
+    @Test
+    public void deveEnviarValorViaQueryViaParam(){
+
+        given()
+                .log().all()
+                .queryParam("format", "xml")
+        .when()
+                .get("https://restapi.wcaquino.me/v2/users")
+        .then()
+                .log().all()
+                .statusCode(200)
+                .contentType(ContentType.XML)
+                .contentType(Matchers.containsString("utf-8"))
         ;
     }
 }
